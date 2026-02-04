756036062433a4a6cd1676c7773e8d1f2a5e2f14
Noob Guy <indiasumitranjan@gmail.com>
2026-02-04 12:16:07 +0530

    Implement applyMove function for game logic

---
+++ src/engine/applyMove.js
@@ -0,0 +1,21 @@
+import { cloneBoard, RK, BK } from "./board"
+
+export function applyMove(board, m) {
+  let b = cloneBoard(board)
+  let [x,y] = m.from
+  let [nx,ny] = m.to
+  let p = b[x][y]
+
+  b[x][y] = 0
+  b[nx][ny] = p
+
+  if (m.cap) {
+    let [cx,cy] = m.cap
+    b[cx][cy] = 0
+  }
+
+  if (p === 'r' && nx === 0) b[nx][ny] = RK
+  if (p === 'b' && nx === 7) b[nx][ny] = BK
+
+  return b
+}
