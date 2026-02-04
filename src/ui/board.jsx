6bab2ad4aa8caaba0cfbb2767094f98a0d53b1ca
Noob Guy <indiasumitranjan@gmail.com>
2026-02-04 12:24:57 +0530

    Refactor Board component to use Piece component

--- src/ui/board.jsx
+++ src/ui/board.jsx
@@ -1,21 +1,23 @@
 import Square from "./Square"
+import Piece from "./Piece"
 
 export default function Board({board,onSquare}) {
   return (
-    <div style={{display:"grid",gridTemplateColumns:"repeat(8,60px)"}}>
-      {board.map((r,i)=>r.map((c,j)=>(
-        <Square
-          key={i+j}
-          dark={(i+j)%2}
-          onClick={()=>onSquare(i,j)}>
-          {c && <div style={{
-            width:40,height:40,
-            borderRadius:"50%",
-            background:c.toLowerCase()==='r'?"red":"black",
-            border:c===c.toUpperCase()?"3px solid gold":"none"
-          }}/>}
-        </Square>
-      )))}
+    <div style={{
+      display:"grid",
+      gridTemplateColumns:"repeat(8,60px)"
+    }}>
+      {board.map((row,i)=>
+        row.map((cell,j)=>(
+          <Square
+            key={`${i}-${j}`}
+            dark={(i+j)%2===1}
+            onClick={()=>onSquare(i,j)}
+          >
+            {cell && <Piece piece={cell}/>}
+          </Square>
+        ))
+      )}
     </div>
   )
 }
